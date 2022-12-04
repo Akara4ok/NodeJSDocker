89189ecbeb12f3a44075d6c4d02ede0a2d826cc6
vlad <vladshpilka86@gmail.com>
2022-12-04 16:51:04 +0200

    add linters

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const http = require('http');
+const http = require("http");
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Hello, World!\n');
-}
+  res.end("Hello, World!\n");
+};
 
 const server = http.createServer(requestListener);
-server.listen(80);
-console.log("Server running\n")
+server.listen(8080);
+console.log("Server running\n");
