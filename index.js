411c37e55806113e25329a489b5460bac3b00653
vlad <vladshpilka86@gmail.com>
2022-12-19 20:26:48 +0200

    add k8s

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const http = require('http');
-const url = require("url");
+const url = require('url');
 
 const requestListener = function (req, res) {
     const reqUrl = url.parse(req.url).pathname;
     res.statusCode = 200;
-    if(req.method == "GET"){
-        if(reqUrl === "/"){
+    if (req.method == 'GET') {
+        if (reqUrl === '/') {
             res.end('Hello, World!\n');
-        }
-        else if(reqUrl === "/liveness-probe")
-        {
+        } else if (reqUrl === '/liveness-probe') {
             res.end('Health!\n');
-    
         }
     }
 };
