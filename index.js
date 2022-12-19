9ebe1fb3a7740c13e2730fe2c5770a3d1ee28e4c
vlad <vladshpilka86@gmail.com>
2022-12-19 20:19:01 +0200

    add k8s

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const http = require('http');
+const url = require("url");
 
 const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end('Hello, World!\n');
+    const reqUrl = url.parse(req.url).pathname;
+    res.statusCode = 200;
+    if(req.method == "GET"){
+        if(reqUrl === "/"){
+            res.end('Hello, World!\n');
+        }
+        else if(reqUrl === "/liveness-probe")
+        {
+            res.end('Health!\n');
+    
+        }
+    }
 };
 
 const server = http.createServer(requestListener);
